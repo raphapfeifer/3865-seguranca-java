bec14d287b43cd6f6d7fac0e4a1c68785dcd724a
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2026-04-27 17:44:01 -0300

    Logout created

--- src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
+++ src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
@@ -38,6 +38,8 @@ public class SecurityConfig {
                 }).formLogin(form -> form.loginPage("/login")
                 .defaultSuccessUrl("/")
                 .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login?logout")
+                        .permitAll())
                 .build();
     }
 
