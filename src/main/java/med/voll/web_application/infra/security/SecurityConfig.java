69eaa899c15943646b41998c98c7b7956f14d5d8
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2026-05-04 14:25:50 -0300

    Add remember me cookie

--- src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
+++ src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package med.voll.web_application.infra.security;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -40,6 +41,10 @@ public class SecurityConfig {
                 .permitAll())
                 .logout(logout -> logout.logoutSuccessUrl("/login?logout")
                         .permitAll())
+
+                .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim")
+                        .alwaysRemember(true))
+                .csrf(Customizer.withDefaults())
                 .build();
     }
 
