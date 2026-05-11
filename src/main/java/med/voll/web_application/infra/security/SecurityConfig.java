4ea949b05bbc76a89f392e29cdf7e5b7068045b7
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2026-05-11 17:00:20 -0300

    Security integration between frontend and backend

--- src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
+++ src/main/java/med/voll/web_application/infra/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,6 +29,11 @@ public class SecurityConfig {
                         .alwaysRemember(true))
                 .csrf(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder condificadorSenha(){
+        return new BCryptPasswordEncoder();
     }
 
 }
